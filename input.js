e51e90d962718ed0ed42ab5713b0579aac97a7d2
Alessandro <alessandro.y.velasquez@gmail.com>
2025-04-10 12:23:13 -0500

    Primer commit

---
+++ input.js
@@ -0,0 +1,14 @@
+let inventario = {};
+
+function agregarProducto() {
+    const p = document.getElementById('producto').value;
+    const c = parseInt(document.getElementById('cantidad').value);
+
+    if (p in inventario) {
+        inventario[p] += c;
+    } else {
+        inventario[p] =c;
+    }
+
+    console.log(inventario)
+}
